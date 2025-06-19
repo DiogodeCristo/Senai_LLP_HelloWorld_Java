46d5b9860aa5af297dad1668c6675a141339e1c9
DiogodeCristo <estudosadsdiogoav@gmail.com>
2025-06-19 16:03:39 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Hello World " + nome);
-        scanner.close();
+        String parametro = (args.length > 0) ? args[0] : "";
+        System.out.println("Hello World " + parametro);
     }
 }
