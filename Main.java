c227b425be78b36e6fbacdf792b5f62c9fe7c001
DiogodeCristo <estudosadsdiogoav@gmail.com>
2025-06-19 16:06:06 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,3 +5,23 @@ public class Main {
         System.out.println("Hello World " + parametro);
     }
 }
+///////
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
+/////////
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Hello World " + nome);
+        scanner.close();
+    }
+}
